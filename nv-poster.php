822494d9881b2bbc536c9cc9a64e5171dd9ccb68
DavidFU <david.fu@harmay.com>
2024-10-22 13:24:12 +0800

    format code

--- nv-poster.php
+++ nv-poster.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * NuVous Poster
  *
@@ -18,12 +19,14 @@
 add_action('plugins_loaded', 'nv_product_poster_initialize');
 
 // Register plugins loaded
-function nv_product_poster_initialize() {
+function nv_product_poster_initialize()
+{
     add_action('woocommerce_before_single_product', 'nv_product_poster_enqueue_scripts');
 }
 
 // Enqueue necessary scripts and styles
-function nv_product_poster_enqueue_scripts() {
+function nv_product_poster_enqueue_scripts()
+{
     if (!function_exists('nv_get_referral_url')) {
         error_log('nv_get_referral_url function not found, nv-referral-code plugin should be installed first.');
         return;
@@ -40,7 +43,7 @@ function nv_product_poster_enqueue_scripts() {
     wp_enqueue_script('qrcode', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.qrcode/1.0/jquery.qrcode.min.js', array('jquery'), null, true);
     wp_enqueue_script('poster-generator', plugin_dir_url(__FILE__) . 'js/poster-generator.js', array('jquery', 'html2canvas', 'qrcode'), '1.0', true);
     wp_enqueue_style('poster-generator-style', plugin_dir_url(__FILE__) . 'css/style.css');
-    
+
     // get current user
     $current_user = wp_get_current_user();
 
@@ -52,4 +55,3 @@ function nv_product_poster_enqueue_scripts() {
         'user_name' => $current_user->user_login
     ));
 }
-?>
